b53f2b54be378a81150922fb335316b3a67fffc7
Burela Surya Prathap <burela@jmangroup.com>
2025-06-24 16:06:48 +0530

    docker-compose

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,10 @@ app.get('/api/message', (req, res) => {
   res.json({ message: "Hello from Node.js backend!" });
 });
 
+app.get('/', (req, res) => {
+  res.json({ message: "Hello from Node.js backend!" });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/message')
+    fetch('http://localhost:3000')
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
